ebd5887549694b182e8c9dd44e7cec1435b41396
matte <matte@LAPTOP-CPT1592C.homenet.telecomitalia.it>
2021-03-17 19:47:26 +0100

    Soluzione punto 2 con due metodi

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -15,6 +15,46 @@ public class Libretto {
 		this.voti.add(v);
 	}
 	
+	// Punto 2, stampa dei voti 25
+	/* public void stampaVotiUguali(int punteggio) {
+		// Il libretto stampa da solo i voti
+		System.out.println();
+	}
+	// Oppure, un metodo più generale: restituiamo una stringa
+	public String votiUguali (int punteggio) {
+		//calcola una stringa che contiene i voti 
+		//sarà poi il chiamante a stamparli
+		//-> stampo solo il nome? oppure tutto il voto.toString()?
+	}
+	*/
+	
+	// Oppure ancora meglio: restituiamo una lista
+	public List<Voto> listaVotiUguali2(int punteggio){
+		// restituisce i voti uguali al criterio
+		// però i voti sono dati e restituiti sotto forma di lista
+		// se dovessi cambiare ciò una volta dovrei cambiare per tutti
+		//1. Possiamo creare una nuova lista i cui inserire i voti uguali
+		List<Voto> risultato = new ArrayList<>();
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v); //v è uno stesso oggetto contenuto in due liste diverse
+			}
+		}
+	return risultato;
+	}
+	
+	//TO DO
+	//oppure per una soluzione più completa
+	public Libretto votiUguali(int punteggio) {
+		Libretto risultato = new Libretto();
+		for (Voto v: this.voti) {
+			if (v.getVoto() == punteggio) {
+				risultato.add(v); 
+			}
+		}
+		return risultato;
+	}
+	
 	public String toString() {
 		String s = "";
 		for (Voto v: this.voti) { //sintassi usata per reiterare sulle liste
